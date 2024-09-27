7a02aa45fb0b455975f9a8c286428c8fab584e40
Amol Walia <amolwalia@Amols-MacBook-Air.local>
2024-09-27 16:13:16 -0700

    Lab 4 update

--- stables.js
+++ stables.js
@@ -17,3 +17,9 @@ console.log(STABLE_MONTHLY_FEE*3);
 console.log("Your Monthly Stable Fee is " + (STABLE_MONTHLY_FEE*3 - 20)); // concatenation
 console.log("whats "+"happenin");
 console.log(`My Horse's name is "${horseName}" and costs $${STABLE_MONTHLY_FEE} to ride per month`);
+
+// Lab FOUR
+horseName = "Nickolas";
+let horseNickname = "Nic";
+console.log(`My Horse's name is ${horseName} but his nickname is ${horseNickname}`);
+console.log("Usually 3 month stay is $" + (STABLE_MONTHLY_FEE*3) + ", but with a 10% discount it would be $" + ((STABLE_MONTHLY_FEE*3)*0.10))
