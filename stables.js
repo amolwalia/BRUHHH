2c1834d2c68d875e8f346399f9bcd028e47d6c6e
Amol Walia <amolwalia@Amols-MacBook-Air.local>
2024-09-27 15:24:50 -0700

    adding info to stables

--- stables.js
+++ stables.js
@@ -1,6 +1,19 @@
+// Lesson-3 Code
 let horseName = "Tatum" ;
 console.log(horseName);
 horseName = "Simba";
 console.log(horseName);
 let horseAge = 3;
 console.log(horseAge);
+let isHorseInside = true;
+console.log(isHorseInside);
+
+// Lesson-4 Code
+console.log(typeof 67848);
+const STABLE_MONTHLY_FEE = 9975;
+console.log(STABLE_MONTHLY_FEE); // Constant declared
+
+console.log(STABLE_MONTHLY_FEE*3);
+console.log("Your Monthly Stable Fee is " + (STABLE_MONTHLY_FEE*3 - 20)); // concatenation
+console.log("whats "+"happenin");
+console.log(`My Horse's name is "${horseName}" and costs $${STABLE_MONTHLY_FEE} to ride per month`);
