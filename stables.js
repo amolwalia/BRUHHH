cd47ff0e49825df3a51b57fe3a988c1e15773f8b
Amol Walia <amolwalia@Amols-MacBook-Air.local>
2024-10-11 16:07:36 -0700

    lesson 6 update

--- stables.js
+++ stables.js
@@ -18,7 +18,7 @@ console.log("Your Monthly Stable Fee is " + (STABLE_MONTHLY_FEE*3 - 20)); // con
 console.log("whats "+"happenin");
 console.log(`My Horse's name is "${horseName}" and costs $${STABLE_MONTHLY_FEE} to ride per month`);
 
-// Lab FOUR
+    // Lab FOUR
 horseName = "Nickolas";
 let horseNickname = "Nic";
 console.log(`My Horse's name is ${horseName} but his nickname is ${horseNickname}`);
@@ -32,9 +32,37 @@ if (visitingHorseName === "Mushroom") {
 } else {
     console.log(horseName + " is lonely");
 }
-
-if (!isHorseInside) {
-    console.log (horseName= " is outside");
+    // Lab 5
+if (isHorseInside) {
+    console.log (horseName = " is outside");
 } else {
     console.log(horseName + " is inside");
 }
+
+// Lesson 6
+let word = "Wordd";
+console.log(word.charAt(2));
+
+let horseNames = [horseName, visitingHorseName, "Fluffy"];
+let horseAges = [horseAge, 23, 18];
+let horseLocation = [isHorseInside, false, true];
+console.log(horseNames.length);
+console.log(horseNames);
+horseNames.push("Bradd");
+console.log(horseNames);
+console.log(horseNames.length);
+console.log(horseNames[2]);
+console.log(horseAges[2]);
+console.log(`Welcome to my stable, there are ${horseNames.length} horses in my stable right now. Their names are ${horseNames[0]}, ${horseNames[1]}, ${horseNames[2]}, ${horseNames[3]}`);
+if(horseLocation[2]) {
+    console.log(horseNames[2] + " is inside");
+}
+
+console.log( );
+    // Lab 6
+    horseLocation.push(true);
+    horseAges.push(86);
+    let horseLocationName = ["inside", "outside", "inside", "inside"] ;
+    console.log(`Do you want to meet the horses in my stable? Well you have to, ITS NOT A CHOICE! 
+I have ${horseNames[0]} right now. There names are ${horseNames[0]}, ${horseNames[1]}, ${horseNames[2]}, ${horseNames[3]}`);
+        console.log(`${horseNames[0]} is ${horseLocationName[0]}, ${horseNames[1]} is ${horseLocationName[1]}, ${horseNames[2]} is ${horseLocationName[2]} and ${horseNames[3]} is ${horseLocationName[3]}`);git 
